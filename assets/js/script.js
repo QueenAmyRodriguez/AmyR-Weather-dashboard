6fe424c002bc97a3dcacb4eeec440088bfbc26f2
Amy Rodriguez <queenamyrodriguez@gmail.com>
2022-04-23 19:29:22 -0700

    added fetch call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var cityFormEL = document.querySelector("#city-form");
 var cityInputEl = document.querySelector("#search-city");
 
-function formSubmitHandler(event) {
+var formSubmitHandler = function(event) {
     event.preventDefault();
 
     var cityName = cityInputEl.value.trim();
@@ -14,4 +14,22 @@ function formSubmitHandler(event) {
     cityInputEl.value = "";
 };
 
+var getWeatherData = function(city) {
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=98f32ee96779d3a327e540ab5b2c4954";
+
+    fetch(apiURL)
+    .then(function(response) {
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function(data) {
+                console.log(data);
+            });
+        }
+    })
+    .catch(function(error) {
+        alert(error);
+    })
+}
+
+getWeatherData('Phoenix');
 cityFormEL.addEventListener("submit", formSubmitHandler);
